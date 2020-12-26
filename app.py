610ab98d442a62db8b552b30bb2643cb353f3a11
mai-codes <maissounksara@gmail.com>
2020-12-25 18:05:36 -0600

    update database models

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 
 class Genre(db.Model):
-  __tablename__ = 'Genre'
-
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
+    __tablename__ = 'Genre'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
 
 # Association tables for Artist to Genre and Venue to Genre
 artist_genre_table = db.Table('artist_genre_table',
@@ -49,61 +49,61 @@ venue_genre_table = db.Table('venue_genre_table',
 
 
 class Venue(db.Model):
-  __tablename__ = 'Venue'
+    __tablename__ = 'Venue'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  address = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  # link the associative table for the m2m relationship with genre
-  genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('venues'))
-  # add missing information
-  website = db.Column(db.String(120))
-  seeking_talent = db.Column(db.Boolean, default=False)
-  seeking_description = db.Column(db.String(120))
-  # Venue is the parent a Show
-  # In the parent is where we put the db.relationship in SQLAlchemy
-  shows = db.relationship('Show', backref='venue', lazy=True) 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    # link the associative table for the m2m relationship with genre
+    genres = db.relationship('Genre', secondary=venue_genre_table, backref=db.backref('venues'))
+    # add missing information
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+    # Venue is the parent a Show
+    # In the parent is where we put the db.relationship in SQLAlchemy
+    shows = db.relationship('Show', backref='venue', lazy=True) 
 
-  def __repr__(self):
-    return f'<Venue {self.id} {self.name}>'
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name}>'
 
 
 class Artist(db.Model):
-  __tablename__ = 'Artist'
+    __tablename__ = 'Artist'
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  # link the associative table for the m2m relationship with genre
-  genres = db.relationship('Genre', secondary=artist_genre_table, backref=db.backref('artists'))
-  # add missing info
-  website = db.Column(db.String(120))
-  seeking_venue = db.Column(db.Boolean, default=False)
-  seeking_description = db.Column(db.String(120))
-  shows = db.relationship('Show', backref='artist', lazy=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    # link the associative table for the m2m relationship with genre
+    genres = db.relationship('Genre', secondary=artist_genre_table, backref=db.backref('artists'))
+    # add missing info
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref='artist', lazy=True)
 
-  def __repr__(self):
-    return f'<Artist {self.id} {self.name}>'
+    def __repr__(self):
+      return f'<Artist {self.id} {self.name}>'
 
 
 class Show(db.Model):
-  __tablename__ = 'Show'
-  id = db.Column(db.Integer, primary_key=True)
-  start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)    # Start time required field
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)  
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)    # Start time required field
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)  
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
 
-  def __repr__(self):
-    return f'<Show {self.id} {self.start_time} artist_id={artist_id} venue_id={venue_id}>'
+    def __repr__(self):
+      return f'<Show {self.id} {self.start_time} artist_id={artist_id} venue_id={venue_id}>'
 
 #----------------------------------------------------------------------------#
 # Filters.
